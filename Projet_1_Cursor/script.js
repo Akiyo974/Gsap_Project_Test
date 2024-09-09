4940f688ff7fa426aec445ccb54e5c1747fb5f48
Christen dijoux <123602948+Akiyo974@users.noreply.github.com>
2024-09-09 00:39:48 -0400

    Add files via upload

---
+++ Projet_1_Cursor/script.js
@@ -0,0 +1,31 @@
+var main = document.querySelector("#main")
+var cursor = document.querySelector("#cursor")
+var imageDiv = document.querySelector("#image")
+
+main.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor, {
+        x: dets.x,
+        y: dets.y,
+        duration: 0.6
+    })
+})
+
+imageDiv.addEventListener("mouseenter", function () {
+
+    cursor.innerHTML = "Voir plus"
+
+    gsap.to(cursor, {
+        scale: 2,
+        backgroundColor: "#fefefe48"
+    })
+})
+
+imageDiv.addEventListener("mouseleave", function () {
+
+    cursor.innerHTML = ""
+
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor: "#fefefe"
+    })
+})
